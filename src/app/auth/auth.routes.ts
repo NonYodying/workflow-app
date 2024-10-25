1c6290fafdfb8c38e3cfc7712e7f3e492f885284
Gearsan <gearsan@MacBook-Pro-khxng-Gearsan.local>
2024-10-25 20:35:56 +0700

    add login page

---
+++ src/app/auth/auth.routes.ts
@@ -0,0 +1,11 @@
+export class AuthRoutes {
+}
+// auth.routes.ts
+import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent, title: 'Login' }
+];
+
+export default routes;
